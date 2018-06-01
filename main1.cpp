96ea3d7cb84cdb752b81fdb5283084bdfe9c29cc
stanislav <vdovych@ucu.edu.ua>
2018-06-01 14:39:26 +0300

    MyShell1

--- main1.cpp
+++ main1.cpp
@@ -5,18 +5,95 @@
 #include <wait.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <libgen.h>
+#include <map>
+#include <fnmatch.h>
 
 #define BUFFERSIZE 4096
 
 
 using namespace std;
 
+char curpath[BUFFERSIZE];
+void margs(const vector<string>& args){
+    if(args[1]=="-h"||args[1]=="--help"){
+        cout<<"HEEEELP!!!!"<<endl;
+        return;
+    }
+    for(auto& e:args){
+        cout<<e<<endl;
+    }
+}
+
+void mcd(const vector<string>& args){
+    if(args[1]=="-h"||args[1]=="--help"){
+        cout<<"HEEEELP!!!!"<<endl;
+        return;
+    }
+    char*tmp=realpath(args[1].c_str(),NULL);
+    if(tmp!=NULL)
+        chdir(tmp);
+    else
+        errno=2;
+}
+void merrno(const vector<string>& args){
+    if(args[1]=="-h"||args[1]=="--help"){
+        cout<<"HEEEELP!!!!"<<endl;
+        return;
+    }
+    cout<<errno;
+}
+void mpwd(const vector<string>& args){
+    if(args[1]=="-h"||args[1]=="--help"){
+        cout<<"HEEEELP!!!!"<<endl;
+        return;
+    }
+    cout<<curpath<<endl;
+
+}
+void mexit(const vector<string>& args){
+    if(args[1]=="-h"||args[1]=="--help"){
+        cout<<"HEEEELP!!!!"<<endl;
+        return;
+    }
+    exit(atoi(args[1].c_str()));
+}
+
+typedef void (*builtin)(const vector<string>&);
+const map<string,builtin> builtins={{"mcd",mcd},{"merrno",merrno},{"mpwd",mpwd},{"mexit",mexit},{"margs",margs}};
+
 
 class Command{
     vector<string> args;
+    void forkexec() {
+        const char* command = args[0].c_str();
+        char* argv[BUFFERSIZE];
+        for (int i = 0; i < args.size(); ++i) {
+            argv[i]=new char[BUFFERSIZE];
+            strcpy(argv[i],args[i].c_str());
+        }
+        pid_t pid = fork();
 
+        if (pid == -1)
+        {
+            std::cerr << "Failed to fork()" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        else if (pid > 0)
+        {
+            int status;
+            waitpid(pid, &status, 0);
+        }
+        else
+        {
+            execvp(command, argv);
+            cerr << "Parent: Failed to execute " << command << " \n\tCode: " << errno << endl;
+            exit(EXIT_FAILURE);   // exec never returns
+        }
+    }
 public:
     Command(string raw_command){
+        raw_command+=" ";
         string curr_comm;
         bool onelapka= false;
         bool twolapka= false;
@@ -27,21 +104,82 @@ public:
                 curr_comm+=ch;
                 escape=false;
             } else if (ch == '\'') {
-                onelapka = !onelapka;
-                replace = false;
+                if(!twolapka) {
+                    onelapka = !onelapka;
+                    replace = false;
+                } else
+                    ch+='\'';
             } else if (ch == '\"') {
-                twolapka = !twolapka;
+                if(!onelapka)
+                    twolapka = !twolapka;
+                else
+                    ch+='\"';
             } else if (ch == '\\') {
                 escape = true;
-            } else
+            } else if (ch == '-'){
+                curr_comm+=ch;
+                replace= false;
+            } else if (isspace(ch)){
+                if(onelapka||twolapka){
+                    curr_comm+=ch;
+                }else{
+                    if(replace&&!args.empty()){
+                        vector<string> matched;
+                        char* dir =new char[BUFFERSIZE];
+                        char* path =new char[BUFFERSIZE];
+                        realpath(curr_comm.c_str(),dir);
+                        realpath(curr_comm.c_str(),path);
+                        dirent** files;
+                        int k=scandir(dirname(dir), &files, 0, alphasort);
+                        for(int i=0;i<k;i++){
+                            if(strcmp(files[i]->d_name,".")!=0&&strcmp(files[i]->d_name,"..")!=0){
+                                if(!fnmatch(basename(path),files[i]->d_name,0)){
+                                    char *tmp = new char[BUFFERSIZE];
+                                    strcpy(tmp,dir);
+                                    strcat(tmp,"/");
+                                    strcat(tmp,files[i]->d_name);
+                                    matched.emplace_back(tmp);
+                                }
+                            }
+                        }
+                        if(matched.empty()){
+                            if(!curr_comm.empty())
+                                args.push_back(curr_comm);
+                        } else{
+                            for(auto& e:matched){
+                                args.push_back(e);
+                            }
+                        }
+                    }else {
+                        if(!curr_comm.empty())
+                            args.push_back(curr_comm);
+                    }
+                    curr_comm = "";
+                    onelapka= false;
+                    twolapka= false;
+                    replace = true;
+                    escape  = false;
+                }
+            }else{
+                curr_comm+=ch;
+            }
         }
-        //TODO: interpret command
-        ///I need to read words
-        ///Work with lapkas
-        ///put correct pathes
     }
     void exec(){
-        //TODO: execute command
+        if(args.empty()){
+            return;
+        }
+        bool b= true;
+        for(auto& buil:builtins){
+            if(args[0]==buil.first){
+                buil.second(args);
+                b= false;
+                break;
+            }
+        }
+        if(b)
+            forkexec();
+
     }
 };
 
@@ -56,8 +194,10 @@ int Init(){
 int main(int argc, char** argv){
     if(Init()<0)
         return -1;
-    char curpath[BUFFERSIZE];
+
     Command* command;
+    char in[BUFFERSIZE];
+
     while (true) {
         getcwd(curpath,BUFFERSIZE);
         cout << curpath << " $ ";
